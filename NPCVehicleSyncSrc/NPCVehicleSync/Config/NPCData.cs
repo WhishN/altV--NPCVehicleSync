3aa5f27b3283974cca33e42e2d10ac54d0f1cc20
WhishN <55228303+WhishN@users.noreply.github.com>
2020-07-12 21:39:59 +0200

    intital upload

---
+++ NPCVehicleSyncSrc/NPCVehicleSync/Config/NPCData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NPCVehicleSync.Config
+{
+    static class NPCData
+    {
+        /// <summary>
+        /// All "Small" Vehicles Beeing used
+        /// The Programm will choose one of these for spaces with < 2.5m radius
+        /// </summary>
+        public static readonly List<string> SmallVehicle = new List<string>()
+        {
+            "asterope",
+            "regina",
+            "primo2",
+            "washington",
+            "emperor",
+            "oracle",
+            "dilettante",
+            "stockade",
+            "blade",
+            "moonbeam",
+            "virgo",
+            "tampa",
+            "voodoo",
+            "rancherxl",
+            "baller",
+            "granger",
+            "utillitruck2",
+            "utillitruck3",
+            "speedo",
+            "surfer",
+            "burrito",
+            "bison3"
+        };
+
+        /// <summary>
+        /// Colors that will be randomly chosen from
+        /// </summary>
+        public static readonly List<byte> SmallVehicleColors = new List<byte>()
+        {
+            15,16,17,18,19,20,43,44,45,56,57,75,76,77,78,79,80,81,108,109,110,122
+        };
+
+        /// <summary>
+        ///  Vehicles that can be used for spots with around 6m diameter for spot
+        /// </summary>
+        public static readonly List<string> BigVehicle = new List<string>()
+        {
+            "trash2", "bus", "airbus", "benson", "pounder", "biff", "phantom", "packer", "mixer2", "rubble"
+        };
+    }
+}
---
+++ NPCVehicleSyncSrc/NPCVehicleSync/Systems/NPCNetOwner.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using AltV.Net;
+using AltV.Net.Async;
+using NPCVehicleSync.Config;
+
+namespace NPCVehicleSync.Systems
+{
+    public class NPCNetOwner : IScript
+    {
+        private static bool IsRunning { get; set; }
+
+        private NPCVehicleHandler Handler { get; set; }
+
+        public NPCNetOwner()
+        {
+            IsRunning = true;
+            this.Handler = new NPCVehicleHandler();
+        }
+
+        [AsyncServerEvent("NPCVehicleSync::Start")]
+        public async Task StartAsync()
+        {
+            Alt.Server.LogColored($"{Global.LogPrefix}Starting To Sync NetOwner Data...");
+            await CheckNetOwner();
+        }
+
+        public async Task CheckNetOwner()
+        {
+            while (Main.Running)
+            {
+                await this.Handler.CheckNetOwnerAsync();
+            }
+        }
+
+        [ServerEvent("NPCVehicleSync::Stop")]
+        public void StopAsync()
+        {
+            IsRunning = false;
+            Alt.Server.LogColored($"{Global.LogPrefix}Stopping System...");
+        }
+    }
+}
