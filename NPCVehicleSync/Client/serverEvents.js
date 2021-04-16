9d224aac2b9fc1cf6eed9a3414bf39ffeb5d4653
WhishN <55228303+WhishN@users.noreply.github.com>
2021-04-16 16:32:06 +0200

    Delete NPCVehicleSync directory
    complete refactor so new Upload

--- NPCVehicleSync/Client/serverEvents.js
+++
@@ -1,8 +0,0 @@
-import * as alt from 'alt-client';
-import { NPCVehHandler } from './npcVehicleHandler.js';
-
-alt.log("[NPCVehicleHandler] Loaded ServerEvents");
-
-alt.onServer("NPCVehicle:NetOwner", (vehicle, state, transfer = undefined) => {
-    NPCVehHandler.updateNetOwner(vehicle, state, transfer)
-});
--- NPCVehicleSync/client.js
+++
@@ -1,6 +0,0 @@
-import './Client/gameEntityCreate.js';
-import './Client/gameEntityDestroy.js';
-import './Client/globalPeds.js';
-import './Client/npcVehicle.js';
-import './Client/npcVehicleHandler.js';
-import './Client/serverEvents.js';
